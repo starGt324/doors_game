3934d700727bf1f9660db0ff9ba1a7b985fc4bbf
wadie <wadir.sobhi@gmail.com>
2025-01-08 15:47:22 +0100

    add saving prg,the game now can be continue until player stop it

--- doors.py
+++ doors.py
@@ -1,63 +1,94 @@
-from playsound import playsound
 import os
-bit_one=1
-bit_tow=0
+import random
+import time
+from playsound import playsound
+losesound=r"lose.wav"
+winsound=r"vic.wav"
+load_score=int
+
+
 def app_ver():
-    app_ver_checker="0.1"
+    app_ver_checker="0.2"
     print("the version of script is ",app_ver_checker)
 #win_sound=playsound(r"win.mp3") couse eror
 #lose_sound=playsound(r"lose.mp3")
-ask_player=input(" is the door open/not : ")
-ask_player.lower()
-if ask_player=="open" or ask_player=="OPEN"  or ask_player=="not" or ask_player=="NOT":
 
-    pass
-else:
-    print("you have 2 anwser open or not to go for next step.")
-    exit(1)
 
-import random
-import time
-print("game start_______")
-print()
-print("ai chose...")
-print()
-time.sleep(2.5)
+while True:
+   
+    ask_player=input(" is the door open/not : ")
+    ask_player.lower()
+    if ask_player=="open" or ask_player=="OPEN"  or ask_player=="not" or ask_player=="NOT":
 
-bot_choise=("open","not")
-
-bot=random.choice(bot_choise)
-print("_ai choise_:",bot)
-print()
-
-if bot=="open" and ask_player=="open":
-    print("you skip the mitrex.")
-    print("you win.")
-    playsound(r"./win.mp3")
+        pass
+    else:
+        print("you have 2 anwser open or not to go for next step.")
+        exit(1)
 
     
-elif bot=="not" and ask_player=="not":
-    print("the mitrex controle you.")
-    playsound(r"./lose.mp3")
+    #game start:
 
+    print("game start_______")
+    print()
+    print("ai chose...")
+    print()
+    time.sleep(1.5)
+    #ai chose:
 
-elif bot=="not" and ask_player=="open":
-    print("the mitrex controle you.")
-    playsound(r"./lose.mp3")
+    bot_choise=("open","not")
 
+    bot=random.choice(bot_choise)
+    print("_ai choise_:",bot)
+    print()
+
+    if bot=="open" and ask_player=="open":
+        print("you skip the mitrex.")
+        print("you win.")
+        #playsound(r"./win.mp3")
+        playsound(winsound)
+        break
+        
+    elif bot=="not" and ask_player=="not":
+        print("the mitrex controle you.")
+        #playsound(r"./lose.mp3")
+        playsound(losesound)
+
+    elif bot=="not" and ask_player=="open":
+        print("the mitrex controle you.")
+        #playsound(r"./lose.mp3")
+        playsound(losesound)
+        
+        
+    elif bot=="open" and ask_player=="not":
+        print("you skip the mitrex.")
+        print("you win.")
+        #playsound(r"./win.mp3")
+        playsound(winsound)
+
+    else:
+        print("god help you.")
+        print()
     
-elif bot=="open" and ask_player=="not":
-    print("you skip the mitrex.")
-    print("you win.")
-    playsound(r"./win.mp3")
+    load_score=load_score+1
 
-else:
-    print("god help you.")
-print()
+#save prog:
 
+    with open("score.txt","w") as f:
+        
+        f.write(str(load_score))
+        #f.write(f"player score:{int(score)}")
+        time.sleep(0.5)
 
-print("game end______")
-time.sleep(2)
+#exit game or realoding:
+  
+    end_game=input("do you want to continu play y/n : ").lower()
 
-
+    if end_game=="y" or end_game=="yes":
+        print("yes sure...")
+        print()
+        print("--rloead game.")
+        time.sleep(2)
+    else:
+        print("game end______")
+        exit(1)
 
